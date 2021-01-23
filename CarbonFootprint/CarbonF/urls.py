ba0ec35920ac550f3752e9592e5a15382bd00d06
jrajan15 <johnny.rajan@gmail.com>
2021-01-23 14:51:11 -0600

    Added base.html

--- CarbonFootprint/CarbonF/urls.py
+++ CarbonFootprint/CarbonF/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="CarbonF-home")
+    path('', views.home, name="Home"),
+    path('calculate', views.calculate, name="Calculate"),
+    path('about', views.about, name="About")
 ]
--- CarbonFootprint/CarbonF/views.py
+++ CarbonFootprint/CarbonF/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+def calculate(request):
+    context = {}
+    return render(request, 'calculate.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
