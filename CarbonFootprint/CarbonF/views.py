c3ddcae3a64be5f452e5dda6905331c6e50064b6
jrajan15 <johnny.rajan@gmail.com>
2021-01-23 14:27:29 -0600

    CSS working

--- CarbonFootprint/CarbonF/views.py
+++ CarbonFootprint/CarbonF/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def home(request):
     context = {}
-    return render(request, 'CarbonF/home.html', context)
+    return render(request, 'home.html', context)
