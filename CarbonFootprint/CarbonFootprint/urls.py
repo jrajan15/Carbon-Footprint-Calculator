02cbf5e2c8f403ba482a855ef1241ed538d86f04
jrajan15 <johnny.rajan@gmail.com>
2021-01-23 17:54:07 -0600

    login/logout done

--- CarbonFootprint/CarbonFootprint/urls.py
+++ CarbonFootprint/CarbonFootprint/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('CarbonF.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
