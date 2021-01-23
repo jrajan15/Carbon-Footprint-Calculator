a433dcd1352d43a58dd2604c80661226250c82ea
jrajan15 <johnny.rajan@gmail.com>
2021-01-23 17:08:16 -0600

    Register Page Complete

--- CarbonFootprint/CarbonFootprint/settings.py
+++ CarbonFootprint/CarbonFootprint/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CarbonF.apps.CarbonfConfig',
+    'Users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CarbonF.apps.CarbonfConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
