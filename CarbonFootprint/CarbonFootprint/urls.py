778b79c8ce10c26d5dc97acc57cbed8e75e4c78c
jrajan15 <johnny.rajan@gmail.com>
2021-01-23 17:22:26 -0600

    Deleted duplicates

--- CarbonFootprint/CarbonFootprint/urls.py
+++ CarbonFootprint/CarbonFootprint/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('CarbonF.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
