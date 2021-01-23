9e02fd923df3acfecf78f5efc73980677cd2b892
Madhava R <madhava.raveendra@gmail.com>
2021-01-24 04:44:41 +0530

    minor changes

--- CarbonFootprint/CarbonF/urls.py
+++ CarbonFootprint/CarbonF/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="Home"),
     path('calculate', views.calculate, name="Calculate"),
-    path('about', views.about, name="About")
+    path('about', views.about, name="About"),
+    path('',views.getResponse)
 ]
